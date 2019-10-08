64267796e93a9e826390c077bb151abaadec1f43
rizho123 <v-riczho@microsoft.com>
2019-10-08 04:01:06 -0700

    Updates

---
+++ precache-manifest.b2c32a5574d1ee569708094eb1057d3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c60070e4e1318438b5d2e848536f84f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "aeed3fca1a2122b9b82f",
+    "url": "/clicky-game/static/css/main.02b044fd.chunk.css"
+  },
+  {
+    "revision": "a55d4c8aa08e597d314f",
+    "url": "/clicky-game/static/js/2.ff6b111b.chunk.js"
+  },
+  {
+    "revision": "aeed3fca1a2122b9b82f",
+    "url": "/clicky-game/static/js/main.745bbf33.chunk.js"
+  },
+  {
+    "revision": "61fe689653f34435d57f",
+    "url": "/clicky-game/static/js/runtime-main.8c94c7f2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clicky-game/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "e7f4f2aa5483a69326d53217327ab3bf",
+    "url": "/clicky-game/static/media/wallpaper.e7f4f2aa.jpg"
+  }
+]);
